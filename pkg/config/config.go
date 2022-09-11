642835656ce70a19012c5c9420ae2c4aa74a8991
unknown <vimalkuriensam@gmail.com>
2022-09-11 20:44:59 +0530

    added function to load environment

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,14 +1,20 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
+
+	"github.com/spf13/viper"
 )
 
 var cfg *Config
 
 type Config struct {
+	Env           map[string]any
 	DataChan      chan any
 	Logger        *log.Logger
 	Response      *JSONResponse
@@ -29,6 +35,7 @@ type ErrorResponse struct {
 
 func Initialize() *Config {
 	cfg = &Config{
+		Env:           make(map[string]any),
 		DataChan:      make(chan any),
 		Logger:        log.New(os.Stdout, "", log.Ldate|log.Ltime),
 		Response:      &JSONResponse{},
@@ -39,4 +46,25 @@ func Initialize() *Config {
 
 func GetConfig() *Config {
 	return cfg
+}
+
+func (config *Config) LoadEnvironment(envStatus string) error {
+	wd, _ := os.Getwd()
+	filePath := filepath.Join(wd, "environment", fmt.Sprintf("%s.env", envStatus))
+	viper.SetConfigFile(filePath)
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("error reading config file %v", err)
+	}
+	if envStatus == "production" {
+		for _, value := range os.Environ() {
+			e := strings.Split(value, "=")
+			k, v := e[0], e[1]
+			config.Env[k] = v
+		}
+	} else {
+		for key, value := range viper.AllSettings() {
+			config.Env[key] = value
+		}
+	}
+	return nil
 }
