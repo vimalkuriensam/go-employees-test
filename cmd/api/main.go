e3ea30dcc5f1af1504974c81ed1719decfb70dd0
unknown <vimalkuriensam@gmail.com>
2022-09-11 20:53:12 +0530

    added flag variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+
+	"github.com/vimalkuriensam/go-employees-test/pkg/config"
+)
+
+const DEFAULT_ENVIRONMENT = "development"
+
+var env string
 
 func main() {
-	fmt.Println("main file")
+	flag.StringVar(&env, "envflag", DEFAULT_ENVIRONMENT, "sets the default environment stage")
+	flag.Parse()
+	cfg := config.Initialize()
+	cfg.LoadEnvironment(env)
+	cfg.Logger.Printf("Server is running on port %v", cfg.Env["port"])
 }
